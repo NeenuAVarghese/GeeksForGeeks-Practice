9e24e827d4650a31c5863c7e778244f6e00ac663
Neenu Ann Varghese <neenuavarghese@gmail.com>
2016-11-23 23:16:06 -0800

    Used Moore's Voting Algorithm

--- MajorityElement.java
+++ MajorityElement.java
@@ -2,15 +2,17 @@ public class MajorityElement{
 	public static void main(String args[]){
 		int array[] = {2,3,4,4,4,4,3,3,2,5,5, 4,4, 4};
 		int count[] = new int[10000];
+		boolean flag = false;
 
 		for(int i = 0; i < array.length; i++){
 			count[array[i]] += 1;
-		}
-
-		for(int j=0; j < count.length; j++){
-			if(count[j] >= (array.length/2)){
-				System.out.println("count = " + count[j] + "i = " + j);
+			if(count[array[i]] >= (array.length/2)){
+				System.out.println("count = " + count[array[i]] + "Var = " + array[i]);
+				flag=true;
 			}
+		}
+		if(!flag){
+			System.out.println("Match Not found !");
 		}
 	}	
 
